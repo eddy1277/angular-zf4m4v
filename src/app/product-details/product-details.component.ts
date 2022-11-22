07770aa6723ed4399156fbf20021a02b85699cf4
eddy1277 <zhujunyan@gmail.com>
2022-11-22 18:57:09 -0500

    3 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent {
-
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    
+  }
 }
